1fafa8fb20a585ee15fe67fe01bfe58cb959bd09
Nisa Eka <nisa.eka.juliana@gmail.com>
2023-08-31 06:17:02 +0700

    Delete gs_codingtest/soal_6/web_perpustakaan directory

--- gs_codingtest/soal_6/web_perpustakaan/function/db.php
+++
@@ -1,9 +0,0 @@
-<?php
-// Kredensial database yang diperlukan
-
-$host     = "localhost";
-$uname    = "root";
-$pass     = "";
-$database = "perpustakaan";
-
-$connect = mysqli_connect($host, $uname, $pass, $database) OR die(mysql_error());
